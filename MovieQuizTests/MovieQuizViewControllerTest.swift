fe63ae1ff6ac78bfaec42e5bac955f0de8829c47
LightKrite <egorpartenko@icloud.com>
2023-11-29 20:18:28 +0100

    Проведен рефактор под MVP Написан тест для convert

---
+++ MovieQuizTests/MovieQuizViewControllerTest.swift
@@ -0,0 +1,64 @@
+//
+//  MovieQuizViewControllerTest.swift
+//  MovieQuizViewControllerTest
+//
+//  Created by Egor Partenko on 29.11.2023.
+//
+
+import XCTest
+@testable import MovieQuiz
+
+final class MovieQuizViewControllerMock: MovieQuizViewControllerProtocol {
+    func show(quiz step: MovieQuiz.QuizStepViewModel) {
+        
+    }
+    
+    func showResult(quiz result: MovieQuiz.QuizResultsViewModel) {
+        
+    }
+    
+    func highlightImageBorder(isCorrectAnswer: Bool) {
+        
+    }
+    
+    func removeImageBorder() {
+        
+    }
+    
+    func showLoadingIndicator() {
+        
+    }
+    
+    func hideLoadingIndicator() {
+        
+    }
+    
+    func buttonsEnabled() {
+        
+    }
+    
+    func buttonsDisabled() {
+        
+    }
+    
+    func showNetworkError(message: String) {
+        
+    }
+    
+    
+}
+
+final class MovieQuizPresenterTests: XCTestCase {
+    func testPresenterConvertModel() throws {
+        let viewControllerMock = MovieQuizViewControllerMock()
+        let sut = MovieQuizPresenter(viewController: viewControllerMock)
+        
+        let emptyData = Data()
+        let question = QuizQuestion(image: emptyData, text: "Question Text", correctAnswer: true)
+        let viewModel = sut.convert(model: question)
+        
+         XCTAssertNotNil(viewModel.image)
+        XCTAssertEqual(viewModel.question, "Question Text")
+        XCTAssertEqual(viewModel.questionNumber, "1/10")
+    }
+}
